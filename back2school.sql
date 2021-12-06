08deb42e94698a03d12a27f93207574325a1d8b4
Diana Haiden <63987203+dianahaiden@users.noreply.github.com>
2021-12-06 15:38:22 -0500

    Update back2school.sql

--- back2school.sql
+++ back2school.sql
@@ -1,44 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Dec 06, 2021 at 09:32 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.4.22
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `back2school`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
 
 CREATE TABLE `cart` (
   `UserID` int(10) NOT NULL,
   `ProductID` int(10) NOT NULL,
   `Quantity` int(10) NOT NULL,
   `CartID` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `payment_option`
---
+) 
 
 CREATE TABLE `payment_option` (
   `id` int(11) NOT NULL,
@@ -51,13 +17,7 @@ CREATE TABLE `payment_option` (
   `card_ccv` varchar(5) DEFAULT NULL,
   `card_expiry` date DEFAULT NULL,
   `cod_id` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product`
---
+)
 
 CREATE TABLE `product` (
   `ProductID` int(10) NOT NULL,
@@ -66,23 +26,13 @@ CREATE TABLE `product` (
   `Image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `shipping`
---
 
 CREATE TABLE `shipping` (
   `id` int(11) NOT NULL,
   `shipping_code` varchar(40) NOT NULL,
   `creation_date` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
--- --------------------------------------------------------
-
---
--- Table structure for table `shop_order`
---
 
 CREATE TABLE `shop_order` (
   `id` int(11) NOT NULL,
@@ -92,86 +42,51 @@ CREATE TABLE `shop_order` (
   `shipping_id` int(11) DEFAULT NULL,
   `total_cost` float(10,2) DEFAULT 0.00,
   `order_date` datetime DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
+) 
 
 CREATE TABLE `user` (
   `UserID` int(10) NOT NULL,
   `Username` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `cart`
+)
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`CartID`);
 
---
--- Indexes for table `payment_option`
---
+
 ALTER TABLE `payment_option`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `product`
---
+
 ALTER TABLE `product`
   ADD PRIMARY KEY (`ProductID`);
 
---
--- Indexes for table `shipping`
---
+
 ALTER TABLE `shipping`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `shop_order`
---
+
 ALTER TABLE `shop_order`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `user`
---
+
 ALTER TABLE `user`
   ADD PRIMARY KEY (`UserID`),
   ADD UNIQUE KEY `Email` (`Email`),
   ADD UNIQUE KEY `Username` (`Username`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `cart`
---
 ALTER TABLE `cart`
   MODIFY `CartID` int(10) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `product`
---
+
 ALTER TABLE `product`
   MODIFY `ProductID` int(10) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `user`
---
+
 ALTER TABLE `user`
   MODIFY `UserID` int(10) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
