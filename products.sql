00cc33053395e3459db180f3ba73ef24d77f5be9
Michelle Anyanwu <83025113+moa38714@users.noreply.github.com>
2021-12-06 16:16:00 -0500

    Update products.sql

--- products.sql
+++ products.sql
@@ -1,12 +1,12 @@
 USE back2school;
 
-INSERT INTO `product` (`productid`, `name`, `price`, `image`, `quantity`, `id`) VALUES
-('1', 'Mechanical Pencil', '3.99', 'images/pencil.jpg', '0', 1),
-('2', 'Pen', '3.99', 'images/pen.jpg', '0', 2),
-('3', 'Notebook', '3.99', 'images/notebook.jpg', '0', 3),
-('4', 'Binder', '3.99', 'images/binder.jpg', '0', 4),
-('5', 'Highlighter', '3.99', 'images/highlighter.jpg', '0', 5),
-('6', 'Post-It Notes', '3.99', 'images/post-it-note.jpg', '0', 6),
-('7', 'BackPack', '3.99', 'images/backpack.jpg', '0', 7),
-('8', 'Laptop', '3.99', 'images/laptop.jpg', '0', 8),
-('9', 'Accessories', '3.99', 'images/accessories.jpg', '0', 9);
+INSERT INTO `product` (`id`, `name`, `price`, `image`, `quantity`) VALUES
+('1', 'Mechanical Pencil', '3.99', 'images/pencil.jpg', '0'),
+('2', 'Pen', '3.99', 'images/pen.jpg', '0'),
+('3', 'Notebook', '3.99', 'images/notebook.jpg', '0'),
+('4', 'Binder', '3.99', 'images/binder.jpg', '0'),
+('5', 'Highlighter', '3.99', 'images/highlighter.jpg', '0'),
+('6', 'Post-It Notes', '3.99', 'images/post-it-note.jpg', '0'),
+('7', 'BackPack', '3.99', 'images/backpack.jpg', '0'),
+('8', 'Laptop', '3.99', 'images/laptop.jpg', '0'),
+('9', 'Accessories', '3.99', 'images/accessories.jpg', '0');
