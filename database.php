fdf5e103bb0271c9eb02a8b5ae273c7e85b7bfd2
Michelle Anyanwu <83025113+moa38714@users.noreply.github.com>
2021-12-06 16:27:32 -0500

    Update database.php

--- database.php
+++ database.php
@@ -4,6 +4,7 @@ $dsn = 'mysql:host=localhost;dbname=back2school';
 $username = 'root';
 $password = '';
 $db = new PDO($dsn,$username,$password);
+$con = mysqli_connect('localhost',$username,$password, 'back2school');
 } catch(PDOException $e) {
   $error_message = $e -> getMessage();
   echo "<p> An error occured while connecting to the database: $error_message </p>";
