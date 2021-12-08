2f369cf413fda5fafbd8cc02692fbb71b36f8152
Michelle Anyanwu <83025113+moa38714@users.noreply.github.com>
2021-12-08 17:09:27 -0500

    Update back2school.sql

--- back2school.sql
+++ back2school.sql
@@ -175,3 +175,17 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+/* insert products into table */
+INSERT INTO `product` (`productid`, `name`, `price`, `image`) VALUES
+('1', 'Mechanical Pencil', '3.99', 'images/pencil.jpg'),
+('2', 'Pen', '3.99', 'images/pen.jpg'),
+('3', 'Notebook', '3.99', 'images/notebook.jpg'),
+('4', 'Binder', '3.99', 'images/binder.jpg'),
+('5', 'Highlighter', '3.99', 'images/highlighter.jpg'),
+('6', 'Post-It Notes', '3.99', 'images/post-it-note.jpg'),
+('7', 'BackPack', '3.99', 'images/backpack.jpg'),
+('8', 'Laptop', '3.99', 'images/laptop.jpg'),
+('9', 'Accessories', '3.99', 'images/accessories.jpg');
