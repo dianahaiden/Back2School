aa37a51b95e073f723f407cd5ac90e60475d206f
Junyi-Pan <63979063+Junyi-Pan@users.noreply.github.com>
2021-12-08 16:56:54 -0500

    Update 5ShoppingCart.php

--- 5ShoppingCart.php
+++ 5ShoppingCart.php
@@ -6,14 +6,16 @@
      
 
     // Get Session and Username
-    $Username = filter_input(INPUT_POST,$_SESSION['Username']);
-    $query1='SELECT * FROM user WHERE Username = :Username';
+    // $Username = filter_input(INPUT_POST,$_SESSION['Username']);
+    $query1="SELECT * FROM user WHERE Username = '".$_SESSION['Username']."'";
     $statement1 = $db -> prepare($query1);
-    $statement1 -> bindValue('Username', $Username);
+    //$statement1 -> bindValue($_SESSION['Username'], $Username);
 	$statement1 -> execute();
-    $user = $statement1 -> fetchAll();
-    //$UserID = $user['UserID'];
+    $users = $statement1 -> fetch();
+    $UserID = $users['UserID'];
     $statement1 -> closeCursor();
+
+    echo $UserID;
     
     //$UserID = filter_input(INPUT_POST, $UserID);
     $ProductID = filter_input(INPUT_POST, 'ProductID');
@@ -30,7 +32,7 @@
 
 <!DOCTYPE html>
 <hmtl>
-    <head>
+<head>
         <title>Shoppingcart</title>
         <link rel="stylesheet" href="styleproducts.css">
     </head>
@@ -59,30 +61,28 @@
                 <th>Name</th>
                 <th>Quantity</th>
                 <th>Price</th>
+                <th>Change Quantity</th>
             </tr>
-        <?php foreach ($Products as $Product) : ?>
-           <tr>
-                <td><?php echo $Product['Name']; ?></td>
-                <td><?php echo $Product['Quantity']; ?></td>
-                <td><?php echo $Product['Price'] * $Product['Quantity']; ?></td>
-                <td><button type="submit">Remove</button></td>
-           </tr>
-         <?php endforeach; ?>
-           <tr>
-                <td>placeholderprice</td>
-                <td>totalprice</td>
-            </tr>
-            <tr>
-               
-            </tr>
+            <form action="AddToCart.php" method="post">
+                <?php foreach ($Products as $Product) : ?>
+                    <tr>
+                        <td><?php echo $Product['Name']; ?></td>
+                        <td><?php echo $Product['Quantity']; ?></td>
+                        <td><?php echo $Product['Price'] * $Product['Quantity']; ?></td>
+                        <td><form action="AddToCart.php" method="post">
+                        <input type="text" name="Quantity">
+						<input type = "submit" value = "Change">
+						<!-- gives three inputs to the AddToCart.php page -->
+                    	<input type="hidden" name="ProductID" value="<?php echo $product['ProductID']; ?>">
+						<input type="hidden" name="UserID" value="<?php echo $UserID?>">
+					    </form></td>
+                    </tr>
+                <?php endforeach; ?>
+            </form>
         </table>
-        <form action="6CheckOut.php">
+        <form action="6CheckOut.php" method="post">
             <input type="submit" value="Checkout">
         </form>
         
     </body>
-
-    <footer>
-		<p>@ 2021 Back2School</p>	
-	</footer>
 </html>
