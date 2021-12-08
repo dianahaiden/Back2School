f1324be2751f34263956e01c28a3c5c66144bd2e
Junyi-Pan <63979063+Junyi-Pan@users.noreply.github.com>
2021-12-08 17:24:28 -0500

    Update 5ShoppingCart.php

--- 5ShoppingCart.php
+++ 5ShoppingCart.php
@@ -8,7 +8,7 @@
     // Get Session and Username
     // $Username = filter_input(INPUT_POST,$_SESSION['Username']);
     $query1="SELECT * FROM user WHERE Username = '".$_SESSION['Username']."'";
-    $statement1 = $db -> prepare($query1);https://github.com/dianahaiden/Back2School/stargazers
+    $statement1 = $db -> prepare($query1);
     //$statement1 -> bindValue($_SESSION['Username'], $Username);
 	$statement1 -> execute();
     $users = $statement1 -> fetch();
@@ -19,10 +19,10 @@
     
     //$UserID = filter_input(INPUT_POST, $UserID);
     $ProductID = filter_input(INPUT_POST, 'ProductID');
-	$query = 'SELECT P.ProductID, P.Name, P.Price, C.Quantity FROM Product P INNER JOIN Cart C ON P.ProductID = C.ProductID WHERE C.UserID = UserID ORDER BY P.ProductID;';
+	$query = "SELECT P.ProductID, P.Name, P.Price, C.Quantity FROM Product P INNER JOIN Cart C ON P.ProductID = C.ProductID WHERE C.UserID = '".$UserID."' ORDER BY P.ProductID;";
 	$statement = $db -> prepare($query);
 	$success = $statement -> execute();
-	$Products = $statement -> fetchAll(PDO::FETCH_ASSOC);
+	$products = $statement -> fetchAll(PDO::FETCH_ASSOC);
 	$statement -> closeCursor();
     
     } else {
@@ -61,21 +61,18 @@
                 <th>Name</th>
                 <th>Quantity</th>
                 <th>Price</th>
-                <th>Change Quantity</th>
             </tr>
-            <form action="AddToCart.php" method="post">
-                <?php foreach ($Products as $Product) : ?>
+            <form action="removeCart.php" method="post">
+                <?php foreach ($products as $product) : ?>
                     <tr>
-                        <td><?php echo $Product['Name']; ?></td>
-                        <td><?php echo $Product['Quantity']; ?></td>
-                        <td><?php echo $Product['Price'] * $Product['Quantity']; ?></td>
-                        <td><form action="AddToCart.php" method="post">
-                        <input type="text" name="Quantity">
-						<input type = "submit" value = "Change">
-						<!-- gives three inputs to the AddToCart.php page -->
-                    	<input type="hidden" name="ProductID" value="<?php echo $product['ProductID']; ?>">
-						<input type="hidden" name="UserID" value="<?php echo $UserID?>">
-					    </form></td>
+                        <td><?php echo $product['Name']; ?></td>
+                        <td><?php echo $product['Quantity']; ?></td>
+                        <td><?php echo $product['Price'] * $product['Quantity']; ?></td>
+                        <td>
+                            <input type="submit" value="Remove">
+                            <input type="hidden" name="ProductID" value="<?php echo $product['ProductID']; ?>">
+						    <input type="hidden" name="UserID" value="<?php echo $UserID; ?>">
+                        </td>
                     </tr>
                 <?php endforeach; ?>
             </form>
