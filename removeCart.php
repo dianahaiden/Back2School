7dcf52382b2748b0928abd0533bff131b16ddad2
Junyi-Pan <63979063+Junyi-Pan@users.noreply.github.com>
2021-12-08 17:25:01 -0500

    Add files via upload

---
+++ removeCart.php
@@ -0,0 +1,14 @@
+<?php
+require_once('database.php');
+
+$ProductID = filter_input(INPUT_POST, 'ProductID');
+$UserID = filter_input(INPUT_POST, 'UserID');
+
+$query = 'DELETE FROM cart WHERE UserID = :UserID AND ProductID = :ProductID';
+$statement = $db->prepare($query);
+$statement -> bindValue('ProductID', $ProductID);
+$statement -> bindValue('UserID', $UserID);
+$success = $statement->execute();
+$statement->closeCursor();
+header("location: 5ShoppingCart.php")
+?>
