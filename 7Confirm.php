a998b6c67673b6750b26b5bad287f0bd3b842c11
Fernanda Bonilla <88325856+fernandabonilla7@users.noreply.github.com>
2021-12-13 22:56:55 -0500

    Update 7Confirm.php

--- 7Confirm.php
+++ 7Confirm.php
@@ -24,3 +24,38 @@
     header('location: 1Login.php');
   }
 ?>
+
+<!DOCTYPE html>
+<hmtl>
+    <head>
+        <title>Search</title>
+        <link rel="stylesheet" href="styleproducts.css">
+    </head>
+
+    <div class="header-bar">
+		<div class="header-left">
+			<p>HAVE FUN SHOPPING!</p>
+		</div>
+		<div class="header-homepage">
+			<a href="3SearchAndSearchResults.php"><h1>BACK2SCHOOL</h1></a>
+		</div>
+		<div class="header-right">
+			<div class="flex1">
+                <a href="signout.php"><p>SIGN OUT</p></a>
+			</div>
+			<div class="flex2">
+				<a href="5ShoppingCart.php"><img src="cart.png" width="60px"></a>
+			</div>
+		</div>
+	</div>
+    
+    <body>
+    <div class="confirm-container">
+        <h2>Thanks for your purchase!</h2>
+    </div>
+    </body>
+
+    <footer>
+		<p>@ 2021 Back2School</p>	
+	</footer>
+</html>
