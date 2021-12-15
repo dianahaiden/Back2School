4c16ce367a5ecf5699ccd02b4d3dfbe21437ea51
Diana Haiden <63987203+dianahaiden@users.noreply.github.com>
2021-12-15 12:09:31 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 15, 2021 at 08:12 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `back2school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `UserID` int(10) NOT NULL,
+  `ProductID` int(10) NOT NULL,
+  `Quantity` int(10) NOT NULL,
+  `CartID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `ProductID` int(10) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Price` float NOT NULL,
+  `Image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`ProductID`, `Name`, `ProductDescription`, `Price`, `Image`) VALUES
+(1, 'MECHANICAL PENCIL', 'An instrument used primarily for symbol creation upon paper through use of graphite trails.', 0.99, 'images/mechanicalpencil.jpg'),
+(2, 'BINDER', 'An item that can be used to store and organize paper for various uses.', 4.99, 'images/binder.jpg'),
+(3, 'BACKPACK', 'An item that allows one to easily carry multiple items and can also aid in keeping track of items.', 15.99, 'images/backpack.jpg'),
+(4, 'HIGHLIGHTER', 'An instrument that can color over a section of writing to allow the reader to more easily identify it.', 1.99, 'images/highlighter.jpg'),
+(5, 'NOTEBOOK', 'An item that contains paper for writing on and also allows all paper to be organized into a singular book.', 3.99, 'images/notebook.jpg'),
+(6, 'PEN', 'An instrument that allows one to write on paper, however it is much darker than graphite and cannot be erased.', 1.99, 'images/pen.jpg'),
+(7, 'POST-IT-NOTE', 'An item that acts as a small piece of paper and can be stuck onto most surfaces and also easily removed.', 0.99, 'images/post-it-note.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`UserID`, `Username`, `Email`, `Password`) VALUES
+(1, '123', '123@gmail.com', '123'),
+(2, '321', '321@gmail.com', '321'),
+(3, '12345', '12345@gmail.com', '12345'),
+(5, '444', '444@gmail.com', '444');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`CartID`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD UNIQUE KEY `Username` (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `ProductID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
